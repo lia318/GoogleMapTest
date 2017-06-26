9b1930557726bec6af7bcba1e279f5936ebdfe3b
sojung kim <sojung kim>
2017-06-26 20:53:02 +0900

    #4 Map+callback

--- app/src/main/java/kr/hs/lia318e_mirim/googlemaptest/MainActivity.java
+++ app/src/main/java/kr/hs/lia318e_mirim/googlemaptest/MainActivity.java
@@ -3,11 +3,29 @@ package kr.hs.lia318e_mirim.googlemaptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap googleMap;
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    } // end of onCreate
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(45.818580, 15.986982), 18));
+        googleMap.getUiSettings().setZoomControlsEnabled(true); // 확대 축소 버튼
+    } // end of onMapReady
+} // end of MainActivity
