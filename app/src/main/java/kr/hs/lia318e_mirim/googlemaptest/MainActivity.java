c0a0572423b91289f11a246340886ebfb54c9ec1
sojung kim <sojung kim>
2017-06-27 19:09:32 +0900

    #5 opotion menu

--- app/src/main/java/kr/hs/lia318e_mirim/googlemaptest/MainActivity.java
+++ app/src/main/java/kr/hs/lia318e_mirim/googlemaptest/MainActivity.java
@@ -2,6 +2,8 @@ package kr.hs.lia318e_mirim.googlemaptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,7 +27,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(45.818580, 15.986982), 18));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(45.818580, 15.986982), 18)); // 줌인 레벨 1~20
         googleMap.getUiSettings().setZoomControlsEnabled(true); // 확대 축소 버튼
     } // end of onMapReady
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, 1, 0, "위성 지도"); // 메뉴 구별 용도
+        menu.add(0, 2, 0, "일반 지도");
+        menu.add(0, 3, 0, "프로방스");
+
+        return true;
+    } // end of onCreateOptionsMenu
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case 1: googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                return true;
+            case 2: googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case 3: googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.791088, 126.685061), 18));
+                return true;
+        } // end of switch
+
+        return false;
+    } // end of onOptionsItemSelected
 } // end of MainActivity
